6c2a3d2e63623e76b176c31e025ccf16c00a7e31
Christopher Tarquini <chris@tarq.io>
2020-01-08 10:31:05 -0500

    Fixed up aggregate stats, add more slave stats
    - Fixed Aggregate stats vs per request stats for new versions of locust
    - Added stats for per slave state and user counts

--- main.go
+++ main.go
@@ -28,8 +28,8 @@ type Exporter struct {
 	mutex sync.RWMutex
 	fetch func(endpoint string) (io.ReadCloser, error)
 
-	locustUp, locustUsers, locustSlaves, locustFailRatio, locustCurrentResponseTimePercentileNinetyFifth, locustCurrentResponseTimePercentileFiftieth                                                                                                                                                        prometheus.Gauge
-	locustNumRequests, locustNumFailures, locustAvgResponseTime, locustNinetiethResponseTime, locustCurrentFailPerSec, locustMinResponseTime, locustMaxResponseTime, locustCurrentRps, locustMedianResponseTime, locustAvgContentLength, locustErrors *prometheus.GaugeVec
+	locustNumRequestsTotal, locustNumFailuresTotal, locustAvgResponseTimeTotal, locustNinetiethResponseTimeTotal, locustCurrentFailPerSecTotal, locustMinResponseTimeTotal, locustMaxResponseTimeTotal, locustCurrentRpsTotal, locustMedianResponseTimeTotal, locustAvgContentLengthTotal, locustUp, locustUsers, locustSlaves, locustFailRatio, locustCurrentResponseTimePercentileNinetyFifth, locustCurrentResponseTimePercentileFiftieth, locustSlaveStateReady, locustSlaveStateHatching, locustSlaveStateRunning, locustSlaveStateCleanup, locustSlaveStateStopping, locustSlaveStateStopped, locustSlaveStateMissing prometheus.Gauge
+	locustNumRequests, locustNumFailures, locustAvgResponseTime, locustNinetiethResponseTime, locustCurrentFailPerSec, locustMinResponseTime, locustMaxResponseTime, locustCurrentRps, locustMedianResponseTime, locustAvgContentLength, locustErrors, locustSlaveUserCount  *prometheus.GaugeVec
 	totalScrapes                                                                                                                                                                                prometheus.Counter
 }
 
@@ -72,6 +72,76 @@ func NewExporter(uri string, timeout time.Duration) (*Exporter, error) {
 				Help:      "The current number of slaves.",
 			},
 		),
+		locustNumRequestsTotal: prometheus.NewGauge(
+			prometheus.GaugeOpts{
+				Namespace: namespace,
+				Subsystem: "requests",
+				Name:      "num_requests_total",
+			},
+		),
+		locustNumFailuresTotal: prometheus.NewGauge(
+			prometheus.GaugeOpts{
+				Namespace: namespace,
+				Subsystem: "requests",
+				Name:      "num_failures_total",
+			},
+		),
+		locustAvgResponseTimeTotal: prometheus.NewGauge(
+			prometheus.GaugeOpts{
+				Namespace: namespace,
+				Subsystem: "requests",
+				Name:      "avg_response_time_total",
+			},
+		),
+		locustNinetiethResponseTimeTotal: prometheus.NewGauge(
+			prometheus.GaugeOpts{
+				Namespace: namespace,
+				Subsystem: "requests",
+				Name:      "ninetieth_response_time_total",
+			},
+		),
+		locustCurrentFailPerSecTotal: prometheus.NewGauge(
+			prometheus.GaugeOpts{
+				Namespace: namespace,
+				Subsystem: "requests",
+				Name:      "current_fail_per_sec_total",
+			},
+		),
+		locustMinResponseTimeTotal: prometheus.NewGauge(
+			prometheus.GaugeOpts{
+				Namespace: namespace,
+				Subsystem: "requests",
+				Name:      "min_response_time_total",
+			},
+		),
+		locustMaxResponseTimeTotal: prometheus.NewGauge(
+			prometheus.GaugeOpts{
+				Namespace: namespace,
+				Subsystem: "requests",
+				Name:      "max_response_time_total",
+			},
+		),
+		locustCurrentRpsTotal: prometheus.NewGauge(
+			prometheus.GaugeOpts{
+				Namespace: namespace,
+				Subsystem: "requests",
+				Name:      "current_rps_total",
+			},
+		),
+		locustMedianResponseTimeTotal: prometheus.NewGauge(
+			prometheus.GaugeOpts{
+				Namespace: namespace,
+				Subsystem: "requests",
+				Name:      "median_response_time_total",
+			},
+		),
+		locustAvgContentLengthTotal: prometheus.NewGauge(
+			prometheus.GaugeOpts{
+				Namespace: namespace,
+				Subsystem: "requests",
+				Name:      "avg_content_length_total",
+			},
+		),
 		locustCurrentResponseTimePercentileNinetyFifth: prometheus.NewGauge(
 			prometheus.GaugeOpts{
 				Namespace: namespace,
@@ -86,7 +156,7 @@ func NewExporter(uri string, timeout time.Duration) (*Exporter, error) {
 				Name:      "current_response_time_percentile_50",
 			},
 		),
-		locustFailRatio: prometheus.NewGauge(
+        locustFailRatio: prometheus.NewGauge(
 			prometheus.GaugeOpts{
 				Namespace: namespace,
 				Subsystem: "requests",
@@ -181,6 +251,62 @@ func NewExporter(uri string, timeout time.Duration) (*Exporter, error) {
 			},
 			[]string{"method", "name"},
 		),
+		locustSlaveStateReady: prometheus.NewGauge(
+			prometheus.GaugeOpts{
+				Namespace: namespace,
+				Subsystem: "slaves",
+				Name:      "state_ready",
+				Help:      "Current count of ready slaves",
+			},
+		),
+		locustSlaveStateHatching: prometheus.NewGauge(
+			prometheus.GaugeOpts{
+				Namespace: namespace,
+				Subsystem: "slaves",
+				Name:      "state_hatching",
+				Help:      "Current count of hatching slaves",
+			},
+		),
+		locustSlaveStateRunning: prometheus.NewGauge(
+			prometheus.GaugeOpts{
+				Namespace: namespace,
+				Subsystem: "slaves",
+				Name:      "state_running",
+				Help:      "Current count of running slaves",
+			},
+		),
+		locustSlaveStateCleanup: prometheus.NewGauge(
+			prometheus.GaugeOpts{
+				Namespace: namespace,
+				Subsystem: "slaves",
+				Name:      "state_cleanup",
+				Help:      "Current count of cleanup slaves",
+			},
+		),
+		locustSlaveStateStopping: prometheus.NewGauge(
+			prometheus.GaugeOpts{
+				Namespace: namespace,
+				Subsystem: "slaves",
+				Name:      "state_stopping",
+				Help:      "Current count of stopping slaves",
+			},
+		),
+		locustSlaveStateStopped: prometheus.NewGauge(
+			prometheus.GaugeOpts{
+				Namespace: namespace,
+				Subsystem: "slaves",
+				Name:      "state_stopped",
+				Help:      "Current count of stopped slaves",
+			},
+		),
+		locustSlaveStateMissing: prometheus.NewGauge(
+			prometheus.GaugeOpts{
+				Namespace: namespace,
+				Subsystem: "slaves",
+				Name:      "state_missing",
+				Help:      "Current count of missing slaves",
+			},
+		),
 		totalScrapes: prometheus.NewCounter(
 			prometheus.CounterOpts{
 				Namespace: namespace,
@@ -188,6 +314,15 @@ func NewExporter(uri string, timeout time.Duration) (*Exporter, error) {
 				Help:      "The total number of scrapes.",
 			},
 		),
+		locustSlaveUserCount: prometheus.NewGaugeVec(
+			prometheus.GaugeOpts {
+				Namespace: namespace,
+				Subsystem: "slaves",
+				Name: "slave_user_count",
+			},
+			[]string {"slave_id"},
+		),
+
 	}, nil
 }
 
@@ -203,6 +338,25 @@ func (e *Exporter) Describe(ch chan<- *prometheus.Desc) {
 	ch <- e.locustFailRatio.Desc()
 	ch <- e.locustCurrentResponseTimePercentileNinetyFifth.Desc()
 	ch <- e.locustCurrentResponseTimePercentileFiftieth.Desc()
+	ch <- e.locustSlaveStateReady.Desc()
+	ch <- e.locustSlaveStateHatching.Desc()
+	ch <- e.locustSlaveStateRunning.Desc()
+	ch <- e.locustSlaveStateCleanup.Desc()
+	ch <- e.locustSlaveStateStopping.Desc()
+	ch <- e.locustSlaveStateStopped.Desc()
+	ch <- e.locustSlaveStateMissing.Desc()
+
+	ch <- e.locustNumRequestsTotal.Desc()
+	ch <- e.locustNumFailuresTotal.Desc()
+	ch <- e.locustAvgResponseTimeTotal.Desc()
+	ch <- e.locustNinetiethResponseTimeTotal.Desc()
+	ch <- e.locustCurrentFailPerSecTotal.Desc()
+	ch <- e.locustMinResponseTimeTotal.Desc()
+	ch <- e.locustMaxResponseTimeTotal.Desc()
+	ch <- e.locustCurrentRpsTotal.Desc()
+	ch <- e.locustMedianResponseTimeTotal.Desc()
+	ch <- e.locustAvgContentLengthTotal.Desc()
+	
 
 	e.locustNumRequests.Describe(ch)
 	e.locustNumFailures.Describe(ch)
@@ -215,6 +369,7 @@ func (e *Exporter) Describe(ch chan<- *prometheus.Desc) {
 	e.locustCurrentRps.Describe(ch)
 	e.locustAvgContentLength.Describe(ch)
 	e.locustErrors.Describe(ch)
+	e.locustSlaveUserCount.Describe(ch)
 }
 
 // Collect function of Exporter
@@ -235,6 +390,7 @@ func (e *Exporter) Collect(ch chan<- prometheus.Metric) {
 	e.locustMedianResponseTime.Collect(ch)
 	e.locustAvgContentLength.Collect(ch)
 	e.locustErrors.Collect(ch)
+	e.locustSlaveUserCount.Collect(ch)
 }
 
 type locustStats struct {
@@ -258,6 +414,12 @@ type locustStats struct {
 		Error      string `json:"error"`
 		Occurences int    `json:"occurences"`
 	} `json:"errors"`
+	Slaves []struct {
+		Id	string `json:"id"`
+		State string `json:"state"`
+		UserCount int `json:"user_count"`
+
+	} `json:"slaves",omitempty`
 	TotalRps                                    float64 `json:"total_rps"`
 	FailRatio                                   float64 `json:"fail_ratio"`
 	CurrentResponseTimePercentileNinetyFifth    float64 `json:"current_response_time_percentile_95"`
@@ -287,13 +449,13 @@ func (e *Exporter) scrape(ch chan<- prometheus.Metric) (up float64) {
 	_ = json.Unmarshal([]byte(bodyAll), &locustStats)
 
 	ch <- prometheus.MustNewConstMetric(e.locustUsers.Desc(), prometheus.GaugeValue, float64(locustStats.UserCount))
-	ch <- prometheus.MustNewConstMetric(e.locustSlaves.Desc(), prometheus.GaugeValue, float64(locustStats.SlaveCount))
+	ch <- prometheus.MustNewConstMetric(e.locustSlaves.Desc(), prometheus.GaugeValue, float64(len(locustStats.Slaves)))
 	ch <- prometheus.MustNewConstMetric(e.locustFailRatio.Desc(), prometheus.GaugeValue, float64(locustStats.FailRatio))
 	ch <- prometheus.MustNewConstMetric(e.locustCurrentResponseTimePercentileNinetyFifth.Desc(), prometheus.GaugeValue, float64(locustStats.CurrentResponseTimePercentileNinetyFifth))
 	ch <- prometheus.MustNewConstMetric(e.locustCurrentResponseTimePercentileFiftieth.Desc(), prometheus.GaugeValue, float64(locustStats.CurrentResponseTimePercentileFiftieth))
 
 	for _, r := range locustStats.Stats {
-		if r.Name != "Total" && r.Name != "//stats/requests" {
+		if r.Name != "Aggregated" && r.Method != "" {
 			e.locustNumRequests.WithLabelValues(r.Method, r.Name).Set(float64(r.NumRequests))
 			e.locustNumFailures.WithLabelValues(r.Method, r.Name).Set(float64(r.NumFailures))
 			e.locustAvgResponseTime.WithLabelValues(r.Method, r.Name).Set(r.AvgResponseTime)
@@ -304,13 +466,42 @@ func (e *Exporter) scrape(ch chan<- prometheus.Metric) (up float64) {
 			e.locustCurrentRps.WithLabelValues(r.Method, r.Name).Set(r.CurrentRps)
 			e.locustMedianResponseTime.WithLabelValues(r.Method, r.Name).Set(r.MedianResponseTime)
 			e.locustAvgContentLength.WithLabelValues(r.Method, r.Name).Set(r.AvgContentLength)
+		} else {
+			ch <- prometheus.MustNewConstMetric(e.locustNumRequestsTotal.Desc(), prometheus.GaugeValue, float64(r.NumRequests))
+			ch <- prometheus.MustNewConstMetric(e.locustNumFailuresTotal.Desc(), prometheus.GaugeValue, float64(r.NumFailures))
+			ch <- prometheus.MustNewConstMetric(e.locustAvgResponseTimeTotal.Desc(), prometheus.GaugeValue, r.AvgResponseTime)
+			ch <- prometheus.MustNewConstMetric(e.locustNinetiethResponseTimeTotal.Desc(), prometheus.GaugeValue, r.NinetiethResponseTime)
+			ch <- prometheus.MustNewConstMetric(e.locustCurrentFailPerSecTotal.Desc(), prometheus.GaugeValue, r.CurrentFailPerSec)
+			ch <- prometheus.MustNewConstMetric(e.locustMinResponseTimeTotal.Desc(), prometheus.GaugeValue, r.MinResponseTime)
+			ch <- prometheus.MustNewConstMetric(e.locustMaxResponseTimeTotal.Desc(), prometheus.GaugeValue, r.MaxResponseTime)
+			ch <- prometheus.MustNewConstMetric(e.locustCurrentRpsTotal.Desc(), prometheus.GaugeValue, r.CurrentRps)
+			ch <- prometheus.MustNewConstMetric(e.locustMedianResponseTimeTotal.Desc(), prometheus.GaugeValue, r.MedianResponseTime)
+			ch <- prometheus.MustNewConstMetric(e.locustAvgContentLengthTotal.Desc(), prometheus.GaugeValue, r.AvgContentLength)
+
 		}
+
+	}
+	stateCounts := map[string]int{"ready":0,"hatching":0,"running":0,"cleanup":0,"stopping":0,"stopped":0,"missing":0}
+
+	for _, s := range locustStats.Slaves {
+		// update state counters
+		if value, ok := stateCounts[s.State]; ok {
+			stateCounts[s.State] = value + 1;
+		}
+		e.locustSlaveUserCount.WithLabelValues(s.Id).Set(float64(s.UserCount))
 	}
 
 	for _, r := range locustStats.Errors {
 		e.locustErrors.WithLabelValues(r.Method, r.Name).Set(float64(r.Occurences))
 	}
 
+	ch <- prometheus.MustNewConstMetric(e.locustSlaveStateReady.Desc(), prometheus.GaugeValue, float64(stateCounts["ready"]))
+	ch <- prometheus.MustNewConstMetric(e.locustSlaveStateHatching.Desc(), prometheus.GaugeValue, float64(stateCounts["hatching"]))
+	ch <- prometheus.MustNewConstMetric(e.locustSlaveStateRunning.Desc(), prometheus.GaugeValue, float64(stateCounts["running"]))
+	ch <- prometheus.MustNewConstMetric(e.locustSlaveStateCleanup.Desc(), prometheus.GaugeValue, float64(stateCounts["cleanup"]))
+	ch <- prometheus.MustNewConstMetric(e.locustSlaveStateStopping.Desc(), prometheus.GaugeValue, float64(stateCounts["stopping"]))
+	ch <- prometheus.MustNewConstMetric(e.locustSlaveStateStopped.Desc(), prometheus.GaugeValue, float64(stateCounts["stopped"]))
+	ch <- prometheus.MustNewConstMetric(e.locustSlaveStateMissing.Desc(), prometheus.GaugeValue, float64(stateCounts["missing"]))
 	return 1
 }
 
